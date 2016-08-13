c804cab20f1f3dafe8efbeb1a86b2feb34281a3d
syrohei <syrohei@gmail.com>
2016-08-14 04:36:56 +0900

    fix

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -10,7 +10,9 @@ function setStatus(message) {
 function refreshBalance() {
   var meta = MetaCoin.deployed();
 
-  meta.getBalance.call(account, {from: account}).then(function(value) {
+  meta.getBalance.call(account, {
+    from: account
+  }).then(function(value) {
     var balance_element = document.getElementById("balance");
     balance_element.innerHTML = value.valueOf();
   }).catch(function(e) {
@@ -27,7 +29,9 @@ function sendCoin() {
 
   setStatus("Initiating transaction... (please wait)");
 
-  meta.sendCoin(receiver, amount, {from: account}).then(function() {
+  meta.sendCoin(receiver, amount, {
+    from: account
+  }).then(function() {
     setStatus("Transaction complete!");
     refreshBalance();
   }).catch(function(e) {
@@ -35,6 +39,84 @@ function sendCoin() {
     setStatus("Error sending coin; see log.");
   });
 };
+
+function initialize() {
+  earth = new WE.map('earth_div', {
+    sky: true,
+    atmosphere: true,
+    zoom: 3,
+    unconstrainedRotation: false,
+    center: [24.355406305811105, 135.01886814587635]
+  });
+
+  /*
+
+  WE.tileLayer('http://data.webglearth.com/natural-earth-color/{z}/{x}/{y}.jpg', {
+    tileSize: 256,
+    tms: true
+  }).addTo(earth);
+
+  WE.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' ,{
+    opacity: 0.6
+  }).addTo(earth);
+
+  WE.tileLayer('https://tileserver.maptiler.com/cassini-terrestrial/{z}/{x}/{y}.jpg', {
+    opacity: 0.1
+  }).addTo(earth);
+  */
+
+  WE.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
+    attribution: 'Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA.',
+    opacity: 0.4
+  }).addTo(earth);
+
+
+
+  var marker = WE.marker([51.5, -0.1], "https://www.coingecko.com/assets/coin-250/ethereum%20classic-8c58eab0a183e0c94a84a17fd9bb488b.png", 30, 60).addTo(earth)
+  marker.bindPopup('<b>Hello world!</br><b>Hello world!</br><b>Hello world!</br><b>Hello world!</br>', 400, true)
+  marker.on('click', function(e) {
+    const data = earth.getCenter()
+    if (data[1] < -0.2 || data[1] > 0.0)
+      earth.panInsideBounds([
+        [51.5, 0.0],
+        [51.5, -0.2]
+      ]);
+  })
+  var options = {
+    color: '#000',
+    opacity: 0.001,
+    fillColor: '#ff0',
+    fillOpacity: 1,
+    weight: 1
+  };
+  polygonB = WE.polygon([
+    [51.5, -0.1],
+    [51.5, -0.1],
+    [50.8, 5.5]
+  ], options).addTo(earth);
+  polygonC = WE.polygon([
+    [51.5, -0.1],
+    [51.5, -0.4],
+    [24.355406305811105, -135.01886814587635]
+  ], options).addTo(earth);
+
+}
+
+
+
+setInterval(function() {
+  const data = polygonB.getPoints()
+  24.355406305811105, 135.01886814587635
+  const diff_lat = data[0].lat - 24.355406305811105
+  const diff_lng = data[0].lng - 135.01886814587635
+  const diff = diff_lat / diff_lng
+  console.log(diff)
+  polygonB.movePoint(0, data[0].lat + diff * 10, data[0].lng + 10)
+  polygonB.movePoint(1, data[1].lat + diff * 10, data[1].lng + 10)
+  polygonB.movePoint(2, data[2].lat + diff * 10, data[2].lng + 10)
+}, 6000)
+
+/*
 
 window.onload = function() {
   web3.eth.getAccounts(function(err, accs) {
@@ -53,4 +135,4 @@ window.onload = function() {
 
     refreshBalance();
   });
-}
+}*/
--- eth.js
+++ eth.js
@@ -9,18 +9,58 @@ var j = 1
 var k = 1
 var i = 1
 var m = 0
-var nodes = []
+var pending = []
 
-getData(k, i, j, m)
-getData(10, i, j, m)
-
-storage.get("1a0f74a2-fe2a-4bd0-9663-f762c16c31a2").then((data) => {
-  if (!data)
-    storage.save("1a0f74a2-fe2a-4bd0-9663-f762c16c31a2", [])
+storage.get("1a0f74a2-fe2a-4bd0-9663-f762c16c31a2").catch((data) => {
+  storage.save("1a0f74a2-fe2a-4bd0-9663-f762c16c31a2", [])
 })
 
+setInterval(() => {
+  save()
 
-function getData(k, i, j, m) {
+}, 100)
+
+const params = function(k, i, j, m) {
+  return {
+    uri: `http://${k}.${i}.${j}.${m}:8545`,
+
+    //uri: `http://54.218.109.153:8545`,
+    method: 'POST',
+    body: '{"id":9999999999,"jsonrpc":"2.0","method":"net_listening","params":[]},"uri":${uri}',
+  }
+}
+
+getData(k, i, j, m , 20)
+getData(21, i, j, m, 40)
+getData(41, i, j, m, 60)
+getData(61, i, j, m, 80)
+getData(81, i, j, m, 100)
+getData(101, i, j, m, 120)
+getData(121, i, j, m, 140)
+getData(141, i, j, m, 160)
+getData(161, i, j, m, 180)
+getData(181, i, j, m, 200)
+getData(201, i, j, m, 220)
+getData(221, i, j, m, 240)
+getData(241, i, j, m, 255)
+
+function save() {
+  console.log(pending);
+  if (pending.length > 0) {
+    const node = pending.pop()
+    storage.get("1a0f74a2-fe2a-4bd0-9663-f762c16c31a2").then((data) => {
+      data.push(node.uri)
+      return storage.save("1a0f74a2-fe2a-4bd0-9663-f762c16c31a2", data)
+    }).then((result) => {
+      console.log(result);
+    }).catch((err) => {
+      console.log(err);
+      pending.push(uri)
+    })
+  }
+}
+
+function getData(k, i, j, m ,limit) {
   setTimeout(function() {
     m++;
     if (m == data) {
@@ -35,27 +75,21 @@ function getData(k, i, j, m) {
       j = 0
       k++
     }
+    if (k> limit)
+      return 0
 
-    getData(k, i, j, m)
-    params = {
-      uri: `http://${k}.${i}.${j}.${m}:8545`,
-      //uri: `http://54.218.109.153:8545`,
-      method: 'POST',
-      body: '{"id":9999999999,"jsonrpc":"2.0","method":"net_listening","params":[]}'
-    }
-    rp(params).then((result) => {
-      if (result.errorMessage)
-        console.log(result.errorMessage);
-      else {
-        const data = JSON.parse(result)
-        if (data.result) {
-          console.log(params.uri);
-          storage.get("1a0f74a2-fe2a-4bd0-9663-f762c16c31a2").then((data) => {
-            data.push(params.uri)
-            storage.save("1a0f74a2-fe2a-4bd0-9663-f762c16c31a2", data)
-          })
-        }
+    getData(k, i, j, m, limit)
+
+
+    const p = new params(k, i, j, m)
+      //console.log(p);
+
+    rp(p).then((response) => {
+      const data = JSON.parse(response)
+      if (data.result) {
+        pending.push(p)
       }
+
     })
 
     //data = web3.isConnected()
